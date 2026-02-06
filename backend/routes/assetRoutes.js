93aac98b9daac83b8a1beeba3cbd81f5f1c28244
Sahan Anjana Hewage <sahananjana63@gmail.com>
2026-02-06 16:30:35 +0530

    Refactor asset routes to remove role checks

--- backend/routes/assetRoutes.js
+++ backend/routes/assetRoutes.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 const assetController = require('../controllers/assetController');
 const { protect } = require('../middleware/authMiddleware');
-const { checkRole } = require('../middleware/roleMiddleware');
 
-router.post('/add', protect, checkRole(['Accountant', 'Director']), assetController.addAsset);
 router.get('/', protect, assetController.getAssets);
-router.post('/depreciate', protect, checkRole(['Accountant']), assetController.calculateDepreciation);
+router.post('/', protect, assetController.addAsset);
+router.delete('/:id', protect, assetController.deleteAsset);
 
 module.exports = router;
