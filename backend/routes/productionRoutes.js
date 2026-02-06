791b1cef25179bce2ed5bb1446c0f6829a053c56
Sahan Anjana Hewage <sahananjana63@gmail.com>
2026-02-06 16:28:52 +0530

    Refactor routes to use double quotes and add order routes

--- backend/routes/productionRoutes.js
+++ backend/routes/productionRoutes.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productionController = require('../controllers/productionController');
-const { protect } = require('../middleware/authMiddleware');
+const productionController = require("../controllers/productionController");
+const { protect } = require("../middleware/authMiddleware");
 
-router.get('/centers', protect, productionController.getCenters);
-router.post('/record', protect, productionController.recordProduction);
-router.get('/history', protect, productionController.getProductionHistory);
+router.get("/centers", protect, productionController.getCenters);
+router.post("/record", protect, productionController.recordProduction);
+router.get("/history", protect, productionController.getProductionHistory);
+router.get("/orders", protect, productionController.getOrders);
+router.post("/orders", protect, productionController.createOrder);
+router.put(
+  "/orders/:id/status",
+  protect,
+  productionController.updateOrderStatus,
+);
 
 module.exports = router;
