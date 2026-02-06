e9a8dade787434d79d6dea0c106b8fb2a0b05c66
Sahan Anjana Hewage <sahananjana63@gmail.com>
2026-02-06 16:48:44 +0530

    Add footer with copyright notice to ReportsHub

--- frontend/src/pages/Accounting/ReportsHub.jsx
+++ frontend/src/pages/Accounting/ReportsHub.jsx
@@ -215,6 +215,9 @@ const ReportsHub = () => {
                     </table>
                 </div>
             </div>
+            <div className="text-center text-gray-400 text-sm mt-10">
+                &copy; 2026 Textile Management System. All rights reserved.
+            </div>
         </div>
     );
 };
