2a5398e2ce2d9a6cc2137ed78e574bda3be044e6
Sahan Anjana Hewage <sahananjana63@gmail.com>
2026-02-06 16:28:18 +0530

    Update quotes in reportRoutes.js to double quotes

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const reportController = require('../controllers/reportController');
-const { protect } = require('../middleware/authMiddleware');
+const reportController = require("../controllers/reportController");
+const { protect } = require("../middleware/authMiddleware");
 
-router.get('/sales', protect, reportController.getSalesReport);
-router.get('/inventory', protect, reportController.getInventoryReport);
-router.get('/production', protect, reportController.getProductionReport);
+router.get("/sales", protect, reportController.getSalesReport);
+router.get("/inventory", protect, reportController.getInventoryReport);
+router.get("/production", protect, reportController.getProductionReport);
+router.get("/dashboard", protect, reportController.getDashboardStats);
 
 module.exports = router;
